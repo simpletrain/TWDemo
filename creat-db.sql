5068dc8a32f6dd3bbb7c81fb012eaa52ee52e2a1
simpletrain <zsy498761554@live.com>
2015-08-17 20:26:28 +0800

    delete schedule-table

--- creat-db.sql
+++ creat-db.sql
@@ -6,13 +6,7 @@ CREATE TABLE IF NOT EXISTS student(
  student_id int(4) not null primary key auto_increment,
  student_name varchar(20) not null,
  student_sex varchar(6) not null ,
- class_id int(4) not null,
- schedule_id int(4) not null
-);
-
-CREATE TABLE IF NOT EXISTS schedule(
- schedule_id int(4) not null primary key auto_increment,
- student_id int(4) not null
+ class_id int(4) not null
 );
 
 CREATE TABLE IF NOT EXISTS class(
@@ -26,9 +20,9 @@ CREATE TABLE IF NOT EXISTS subject(
  UNIQUE (subject_name)
 );
 
-CREATE TABLE IF NOT EXISTS schedule_subject(
- schedule_subject_id int(4) not null primary key auto_increment,
- schedule_id int(4) not null,
+CREATE TABLE IF NOT EXISTS student_subject(
+ student_subject int(4) not null primary key auto_increment,
+ student_id int(4) not null,
  subject_id int(4) not null
 );
 
@@ -37,17 +31,12 @@ ADD CONSTRAINT fk_student_class
 FOREIGN KEY (class_id)
 REFERENCES class(class_id);
 
-ALTER TABLE student
-ADD CONSTRAINT fk_student_schedule
-FOREIGN KEY (schedule_id)
-REFERENCES schedule(schedule_id);
+ALTER TABLE student_subject
+ADD CONSTRAINT fk_student_student_subject
+FOREIGN KEY (student_id)
+REFERENCES student(student_id);
 
-ALTER TABLE schedule_subject
-ADD CONSTRAINT fk_subject_schedule_subject
+ALTER TABLE student_subject
+ADD CONSTRAINT fk_subject_student_subject
 FOREIGN KEY (subject_id)
 REFERENCES subject(subject_id);
-
-ALTER TABLE schedule_subject
-ADD CONSTRAINT fk_schedule_schedule_subject
-FOREIGN KEY (schedule_id)
-REFERENCES schedule(schedule_id);
--- drop-db.sql
+++ drop-db.sql
@@ -1,19 +1,16 @@
 USE TWDemo;
 
 ALTER TABLE student DROP FOREIGN KEY fk_student_class;
-ALTER TABLE student DROP FOREIGN KEY fk_student_schedule;
-ALTER TABLE schedule_subject DROP FOREIGN KEY fk_subject_schedule_subject;
-ALTER TABLE schedule_subject DROP FOREIGN KEY fk_schedule_schedule_subject;
+ALTER TABLE student_subject DROP FOREIGN KEY fk_student_student_subject;
+ALTER TABLE student_subject DROP FOREIGN KEY fk_subject_student_subject;
 
 DROP TABLE student;
 
 DROP TABLE class;
 
-DROP TABLE schedule;
-
 DROP TABLE subject;
 
-DROP TABLE schedule_subject;
+DROP TABLE student_subject;
 
 USE mysql;
 
